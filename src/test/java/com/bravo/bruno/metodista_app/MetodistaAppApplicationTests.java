350e4c4d587f4bf255d130465cbcb35c8fb59968
Bruno Bravo <bsbravo@hotmail.com>
2018-10-15 15:14:43 -0300

    First commit

---
+++ src/test/java/com/bravo/bruno/metodista_app/MetodistaAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bravo.bruno.metodista_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MetodistaAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
