a2238f074f3d4fc9dc3adf4f308d391d4d81809a
Bruno Bravo <bsbravo@hotmail.com>
2018-10-19 10:11:57 -0300

    Adding skeleton for integration tests

---
+++ src/test/java/com/bravo/bruno/metodista/podcast/EpisodeIntegrationTests.java
@@ -0,0 +1,22 @@
+package com.bravo.bruno.metodista.podcast;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class EpisodeIntegrationTests {
+	
+	@Autowired
+	TestEntityManager entityManager;
+	
+	@Test
+	public void contextLoads() {
+		System.out.println(entityManager);
+	}
+
+}
--- src/test/java/com/bravo/bruno/metodista/podcast/MetodistaApplicationTests.java
+++ src/test/java/com/bravo/bruno/metodista/podcast/MetodistaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bravo.bruno.metodista_app;
+package com.bravo.bruno.metodista.podcast;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MetodistaApplicationTests {
-
+	
 	@Test
 	public void contextLoads() {
 	}
