b7600f035e0106d5817a170a474a5895e5d3326e
Bruno Bravo <bsbravo@hotmail.com>
2018-10-24 21:24:01 -0300

    cleaning imports

--- src/main/java/com/bravo/bruno/metodista/podcast/episode/EpisodeController.java
+++ src/main/java/com/bravo/bruno/metodista/podcast/episode/EpisodeController.java
@@ -2,13 +2,7 @@ package com.bravo.bruno.metodista.podcast.episode;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EpisodeController {
