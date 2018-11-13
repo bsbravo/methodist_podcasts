f5abd9c5fe1ad85c3a501293376fc8dc9c9579bd
Bruno Bravo <bsbravo@hotmail.com>
2018-11-13 14:58:55 -0200

    adding archetype

---
+++ src/main/java/com/bravo/bruno/methodist/podcast/PodcastApplication.java
@@ -0,0 +1,12 @@
+package com.bravo.bruno.methodist.podcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodcastApplication.class, args);
+	}
+}
