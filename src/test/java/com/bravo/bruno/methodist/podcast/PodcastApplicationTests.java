485f694d5d3048b35933f9074eb5bc3d9eb5355d
Bruno Bravo <bsbravo@hotmail.com>
2018-11-13 15:46:32 -0200

    initializing project with thymeleaf

---
+++ src/test/java/com/bravo/bruno/methodist/podcast/PodcastApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bravo.bruno.methodist.podcast;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PodcastApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
